b2f18fc980fc9d47914de5d4a5a2b6e2e8e1c4f8
wwilliewells <wwilliewells@gmail.com>
2017-05-11 18:55:56 -0400

    updated app page

---
+++ js/convert.js
@@ -0,0 +1,18 @@
+(function(){
+  var units = document.querySelector('#units');
+  var value = document.querySelector('#value');
+
+  var s = document.querySelector('#convert');
+  s.addEventListener('click',function(e){
+    e.stopPropagation();
+    // crazy long logic
+  //   if(fn.value.length > 0){ c.name = fn.value; }
+  //   else{ console.log('no name'); return; }
+  //   if(email.value.length > 0){ c.email = email.value; }
+  //   else{ console.log('no email'); return; }
+  //   if(msg.value.length > 0){ c.message = msg.value; }
+  //   else{ console.log('no message'); return; }
+
+  //   console.log(c.name,c.email,c.message);
+  });
+})();
