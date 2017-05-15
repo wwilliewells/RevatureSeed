6b3258fa39cd4129d948f3f1a70121e13f3ca45b
wwilliewells <wwilliewells@gmail.com>
2017-05-15 19:57:40 -0400

    updated app page, conversion works but output is not persistent

--- src/program.cs
+++ src/program.cs
@@ -9,11 +9,11 @@ namespace willieSeed{
     }
     public static void PlaySquare(){
       //var s = new Square(){Width = 1.1};
-      var s1 = new Square(3);
+      var s1 = new Square(2.3);
       //Square s2(2.3);
 
       sc.WriteLine(s1.Perimeter());
-      sc.WriteLine("s1.Area()");
+      sc.WriteLine("Area: {0:F}, Perimeter: {1:F} ",s1.Area(),s1.Perimeter());
     }
   }
 }
